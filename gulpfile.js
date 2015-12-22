3b0640a1d428159ebdb665df15f0bda9289c172f
John Ashenden <jaashenden@gmail.com>
2015-12-22 18:19:19 -0500

    Cleaning up Gulpfile and Dir Structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 // Dependencies
+
 var autoprefixer = require("gulp-autoprefixer");
 var colorLighten = require("less-color-lighten");
 var gulp = require("gulp");
@@ -9,99 +10,146 @@ var util = require("gulp-util");
 var rename = require("gulp-rename");
 var uglify = require("gulp-uglify");
 
-// Path Defintions
-var dirs = {
-  dist: "./build",
-  src: "./src",
+// Path Definitions
 
-  docsSrc: "./docs",
-  docsStyles: "./docs/styles",
-  docsJavascripts: "./docs/javascripts",
-  docsDist: "./docs/build",
-  docsVendor: "./docs/vendor/**/*.*",
-  docsImages: "./docs/images/**/*.*"
+var dirs = {
+  canvas: {
+    path: "./",
+    styles: "./styles",
+    javascripts: "./javascripts",
+    dist: {
+      path: "./build"
+    }
+  },
+  docs: {
+    path: "./docs",
+    styles: "./docs/styles",
+    javascripts: "./docs/javascripts",
+    vendor: "./docs/vendor/**/*.*",
+    images: "./docs/images/**/*.*",
+    dist: {
+      path: "./docs/build"
+    }
+  }
 };
+
+// File Definitions
 
 var files = {
-  lessMain: "canvas",
-
-  docsLessMain: "docs",
-  docsScripts: "scripts",
-  docsScriptsDist: "scripts",
-  docsScriptsDistSuffix: ".min",
-  docsHTML: dirs.docsSrc + "/" + "index.html"
+  canvas: {
+    styles: "canvas"
+  },
+  docs: {
+    styles: "docs",
+    javascripts: "scripts",
+    html: dirs.docs.path + "/" + "index.html",
+    dist: {
+      javascripts: {
+        filename: "scripts",
+        suffix: ".min"
+      }
+    }
+  }
 };
 
-// Gulp Tasks
+
+
 gulp.task("html", function () {
-  return gulp.src(files.docsHTML)
-    .pipe(gulp.dest(dirs.docsDist));
+
+  return gulp.src(files.docs.html).pipe(gulp.dest(dirs.docs.dist.path));
+
 });
+
+// Move files to a new location
 
 gulp.task("move", function () {
-  // the base option sets the relative root for the set of files,
-  // preserving the folder structure
-  gulp.src([dirs.docsVendor, dirs.docsImages], {base: dirs.docsSrc})
-  .pipe(gulp.dest(dirs.docsDist));
+
+  // the base option sets the relative root for the set of files, preserving the folder structure
+  gulp.src([dirs.docs.vendor, dirs.docs.images], {base: dirs.docs.path}).pipe(gulp.dest(dirs.docs.dist.path));
+
 });
+
+// Process LESS files
 
 gulp.task("less", function () {
-  return gulp.src(dirs.src + "/" + files.lessMain + ".less")
+
+  return gulp.src(dirs.canvas.styles + "/" + files.canvas.styles + ".less")
     .pipe(sourcemaps.init())
     .pipe(less({
-      paths: [dirs.src], // @import paths
+      paths: [dirs.canvas.styles],
       plugins: [colorLighten]
     }))
     .on("error", function (err) {
       util.log(err.message);
       this.emit("end");
     })
-    .pipe(autoprefixer({browsers: ["last 2 versions"]}))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions"]
+    }))
     .pipe(minifyCSS())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(dirs.dist));
+    .pipe(gulp.dest(dirs.canvas.dist.path));
+
 });
 
+// Process Docs LESS files
+
 gulp.task("docs:less", function () {
-  return gulp.src(dirs.docsStyles + "/" + files.docsLessMain + ".less")
+
+  return gulp.src(dirs.docs.styles + "/" + files.docs.styles + ".less")
     .pipe(sourcemaps.init())
     .pipe(less({
-      paths: [dirs.docsSrc], // @import paths
+      paths: [dirs.docs.styles],
       plugins: [colorLighten]
     }))
     .on("error", function (err) {
       util.log(err.message);
       this.emit("end");
     })
-    .pipe(autoprefixer({browsers: ["last 2 versions"]}))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions"]
+    }))
     .pipe(minifyCSS())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(dirs.docsDist));
+    .pipe(gulp.dest(dirs.docs.dist.path));
+
 });
+
+// Process Docs JS files
 
 gulp.task("docs:js", function () {
 
-  return gulp.src(dirs.docsJavascripts + "/" + files.docsScripts + ".js")
+  return gulp.src(dirs.docs.javascripts + "/" + files.docs.javascripts + ".js")
     .pipe(uglify({
       mangle: true,
       compress: true
     }))
     .pipe(rename({
-      basename: files.docsScriptsDist,
-      suffix: files.docsScriptsDistSuffix,
+      basename: files.docs.dist.javascripts.filename,
+      suffix: files.docs.dist.javascripts.suffix,
       extname: ".js"
     }))
-    .pipe(gulp.dest(dirs.docsDist));
+    .pipe(gulp.dest(dirs.docs.dist.path));
 
 });
+
+// Watch for file changes
 
 gulp.task("watch", function () {
+
   gulp.watch("./**/*.less", ["docs:less"]);
-  gulp.watch(files.docsHTML, ["html"]);
+  gulp.watch(files.docs.html, ["html"]);
+
 });
+
+// Distribution Gulp Task
 
 gulp.task("dist", ["less"]);
 
+// Docs Distribution Gulp Task
+
 gulp.task("docs:dist", ["docs:less", "docs:js", "html", "move"]);
+
+// Default Gulp Task
 
 gulp.task("default", ["docs:dist", "watch"]);
